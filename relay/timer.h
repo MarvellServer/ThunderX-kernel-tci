71cb74af668afef9fc30b17997c129e16a78e020
Geoff Levand <geoff@infradead.org>
2019-02-08 11:01:21 -0800

    Add relay server

---
+++ relay/timer.h
@@ -0,0 +1,27 @@
+/*
+ *  TCI relay.
+ */
+
+#ifndef _TIMER_H
+#define _TIMER_H
+
+//#include <stdbool.h>
+#include <time.h>
+#include "list.h"
+
+typedef void (*timer_callback)(void *data);
+
+struct timer {
+	struct list_entry list_entry;
+	time_t timeout;
+	timer_callback callback;
+	void *callback_data;
+};
+
+struct timer *timer_add(unsigned int seconds, timer_callback cb, void *cb_data);
+void timer_start(void);
+void timer_remove(struct timer *entry);
+void __attribute__((unused)) timer_dump_list(const char *text);
+time_t time_left(struct timer *entry);
+
+#endif /* _TIMER_H */
